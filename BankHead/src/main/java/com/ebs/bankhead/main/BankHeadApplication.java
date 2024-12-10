51faf84bdd3983ae3b5808cf29144b1ff8482dd1
saurabhanimje <nimjesaurabh1998@gmail.com>
2024-12-10 14:11:24 +0530

    by Saurabhh

--- BankHead/src/main/java/com/ebs/bankhead/main/BankHeadApplication.java
+++ BankHead/src/main/java/com/ebs/bankhead/main/BankHeadApplication.java
@@ -2,11 +2,13 @@ package com.ebs.bankhead.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BankHeadApplication 
 {
 

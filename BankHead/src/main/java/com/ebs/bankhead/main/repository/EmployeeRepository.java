a3022151100a5f81f07758d61343531decf76995
HP <vishalmorey1998@gmail.com>
2025-01-31 20:10:54 +0530

    by change username $passw

--- BankHead/src/main/java/com/ebs/bankhead/main/repository/EmployeeRepository.java
+++ BankHead/src/main/java/com/ebs/bankhead/main/repository/EmployeeRepository.java
@@ -1,11 +1,15 @@
 package com.ebs.bankhead.main.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ebs.bankhead.main.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> 
+{
 
+	public Optional<Employee> findByEmployeeUsernameAndEmployeePassword(String username,String password);
 }
